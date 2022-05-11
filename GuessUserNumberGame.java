518f8b212ce99a9cd476eace55c48cca6860da86
Amit Dnyandeo Haral <amitharal04@gmail.com>
2022-05-11 22:08:25 +0530

    Guess User Number Game...This game will find out the number you are thinking.

--- GuessUserNumberGame.java
+++ GuessUserNumberGame.java
@@ -3,20 +3,22 @@ package com.bridgelab.Test;
 import java.util.Scanner;
 
 public class GuessUserNumberGame {
-	static Scanner scan = new Scanner(System.in);
-	static int low = 0;
-	static int high = 100;
-	static int mid = (low + high) / 2;
-	static String playerInput;
 
 	public static void guessNumber() {
+		Scanner scan = new Scanner(System.in);
+		int low = 0;
+		int high = 100;
+		int mid = (low + high) / 2;
+		String playerInput;
+
 		do {
-			System.out.println("\nIs this your numb "+ mid +" type 'yes' else if your numb is high type 'high' or else 'low' =");
+			System.out.println("\nIs this your number ->| "+ mid +" |<- type 'yes' else if your numb is high type 'high' or else 'low' =");
 			playerInput=scan.next();
 
 			if(playerInput.equals("yes")) {
 				break;
 			}
+
 			if(playerInput.equals("high")) {
 				low = mid;
 				mid = (low + high) / 2;
@@ -25,14 +27,19 @@ public class GuessUserNumberGame {
 				low = mid / 2;
 				mid = (low + high) / 2;
 			}
-		}while(playerInput!= "yes");
+			
+		}while(playerInput != "yes");
 
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Guess Number between 0-100");
 		System.out.println("Rules for Game:");
-		System.out.println("To interact With your game: \n Please type 'yes' if your number matches \n Or type 'high' if your number is higher than the given number \n Or type 'low' if your number is lower than the given number");
+		System.out.println("To interact With your game: Follow these steps to play");
+		System.out.println("-> Please type 'yes' if your number matches");
+		System.out.println("-> Or type 'high' if your number is higher than the given number");
+		System.out.println("-> Or type 'low' if your number is lower than the given number");
+
 		guessNumber();
 	}
 
